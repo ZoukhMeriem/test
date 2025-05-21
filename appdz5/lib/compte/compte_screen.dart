7d542d8df1d6953ed715dfb2725be6cb2448b2b3
ZoukhMeriem <zoukhmeriem01@gmail.com>
2025-05-21 15:35:25 +0200

    Update compte_screen.dart

--- appdz5/lib/compte/compte_screen.dart
+++ appdz5/lib/compte/compte_screen.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Ne pas oublier la localisation
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../main.dart';
 import 'user_info_page.dart';
@@ -35,11 +35,10 @@ class _ProfilePageState extends State<ProfilePage> {
 
   final Color primaryColor = const Color(0xFF1E1E1E);
 
-
   final List<Color> gradientColors = [
+    Color(0xFFF0F4F8),
+    Color(0xFFD1D9E6),
     Color(0xFFA3BED8),
-    Color(0xFFD1D9E6) ,
-
   ];
 
   final Color iconColor = Colors.black;
@@ -120,181 +119,157 @@ class _ProfilePageState extends State<ProfilePage> {
 
     return Scaffold(
       backgroundColor: isDark ? Colors.black : Colors.white,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          elevation: 0,
-          backgroundColor: Colors.transparent, // Important pour le dégradé
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: gradientColors,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: gradientColors,
+              begin: Alignment.topCenter, // ← Verticale
+              end: Alignment.bottomCenter,
             ),
           ),
         ),
-      ),
-
-      body: Column(
-        children: [
-          _buildHeader(isDark, local),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: isDark
-                      ? [Colors.black87, Colors.black54]
-                      : [Colors.white, Colors.white],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
+        toolbarHeight: 150,
+        title: Column(
+          children: [
+            const SizedBox(height: 20),
+            const CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage('assets/images/logo3.png'),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              userData?['username'] ?? local.unknownName,
+              style: TextStyle(
+                color: primaryColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              child: ListView(
-                padding: const EdgeInsets.all(16),
-                children: [
-                  _buildListTile(Icons.person, local.personalInfo, () {
-                    if (userData != null) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => UserInfoPage(userData: userData!),
-                        ),
-                      );
-                    }
-                  }, isDark),
-                  SwitchListTile(
-                    title: Text(local.darkMode,
-                        style: TextStyle(color: isDark ? Colors.white : Colors.black)),
-                    value: _isDark,
-                    onChanged: (value) {
-                      setState(() {
-                        _isDark = value;
-                      });
-                      toggleTheme(value);
-                    },
-                    secondary: Icon(Icons.dark_mode, color: iconColor),
+            ),
+            Text(
+              userData?['email'] ?? local.unknownEmail,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: isDark
+                ? [Colors.black87, Colors.black54]
+                : [Colors.white, Colors.white],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            _buildListTile(Icons.person, local.personalInfo, () {
+              if (userData != null) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => UserInfoPage(userData: userData!),
                   ),
-                  _buildListTile(Icons.settings, local.settings, () {
-                    if (userData != null && userData!['username'] != null) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SettingsPage(
-                            userData: userData!,
-                            changeLanguage: widget.changeLanguage,
-                            toggleTheme: widget.toggleTheme,
-                            toggleNotifications: widget.toggleNotifications,
-                            selectedLanguage: _language,
-                            isDarkMode: _isDark,
-                          ),
-                        ),
-                      );
-                    }
-                  }, isDark),
-                  _buildListTile(Icons.notifications, local.notifications, () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(local.notificationsComingSoon)),
-                    );
-                  }, isDark),
-                  _buildListTile(Icons.history, local.favoriteTrips, () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const HistoriqueTrajetsPage()),
-                    );
-                  }, isDark),
-                  _buildListTile(Icons.train, local.trainSchedules, () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HoraireTrainPage()),
-                    );
-                  }, isDark),
-                  _buildListTile(Icons.help_outline, local.support, () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const SupportPage()),
-                    );
-                  }, isDark),
-                  const Divider(),
-                  _buildListTile(Icons.logout, local.logout, () async {
-                    final confirmed = await showDialog<bool>(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Text(
-                          local.logout,
-                          style: TextStyle(color: isDark ? Colors.white : Colors.black),
-                        ),
-                        content: Text(local.logoutConfirmation),
-                        actions: [
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, false),
-                            child: Text(local.cancel),
-                          ),
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, true),
-                            child: Text(local.logout),
-                          ),
-                        ],
-                      ),
-                    );
-                    if (confirmed == true) {
-                      await logout(context);
-                    }
-                  }, isDark),
-                ],
-              ),
+                );
+              }
+            }, isDark),
+            SwitchListTile(
+              title: Text(local.darkMode,
+                  style: TextStyle(color: isDark ? Colors.white : Colors.black)),
+              value: _isDark,
+              onChanged: (value) {
+                setState(() {
+                  _isDark = value;
+                });
+                toggleTheme(value);
+              },
+              secondary: Icon(Icons.dark_mode, color: iconColor),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildHeader(bool isDark, AppLocalizations local) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 20),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: gradientColors,
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+            _buildListTile(Icons.settings, local.settings, () {
+              if (userData != null && userData!['username'] != null) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsPage(
+                      userData: userData!,
+                      changeLanguage: widget.changeLanguage,
+                      toggleTheme: widget.toggleTheme,
+                      toggleNotifications: widget.toggleNotifications,
+                      selectedLanguage: _language,
+                      isDarkMode: _isDark,
+                    ),
+                  ),
+                );
+              }
+            }, isDark),
+            _buildListTile(Icons.notifications, local.notifications, () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(local.notificationsComingSoon)),
+              );
+            }, isDark),
+            _buildListTile(Icons.history, local.favoriteTrips, () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const HistoriqueTrajetsPage()),
+              );
+            }, isDark),
+            _buildListTile(Icons.train, local.trainSchedules, () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HoraireTrainPage()),
+              );
+            }, isDark),
+            _buildListTile(Icons.help_outline, local.support, () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SupportPage()),
+              );
+            }, isDark),
+            const Divider(),
+            _buildListTile(Icons.logout, local.logout, () async {
+              final confirmed = await showDialog<bool>(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text(
+                    local.logout,
+                    style:
+                    TextStyle(color: isDark ? Colors.white : Colors.black),
+                  ),
+                  content: Text(local.logoutConfirmation),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, false),
+                      child: Text(local.cancel),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, true),
+                      child: Text(local.logout),
+                    ),
+                  ],
+                ),
+              );
+              if (confirmed == true) {
+                await logout(context);
+              }
+            }, isDark),
+          ],
         ),
       ),
-      child: Column(
-        children: [
-          const CircleAvatar(
-            radius: 30,
-            backgroundImage: AssetImage('assets/images/logo3.png'),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            userData?['username'] ?? local.unknownName,
-            style: TextStyle(
-              color: isDark ? Colors.white : primaryColor,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            userData?['email'] ?? local.unknownEmail,
-            style: TextStyle(
-              color: isDark ? Colors.white70 : Colors.black,
-              fontSize: 14,
-            ),
-          ),
-        ],
-      ),
     );
   }
 
-  Widget _buildListTile(IconData icon, String title, VoidCallback onTap, bool isDark) {
+  Widget _buildListTile(
+      IconData icon, String title, VoidCallback onTap, bool isDark) {
     return ListTile(
       leading: Icon(icon, color: iconColor),
       title: Text(
@@ -305,7 +280,8 @@ class _ProfilePageState extends State<ProfilePage> {
           color: isDark ? Colors.white : Colors.black,
         ),
       ),
-      trailing: Icon(Icons.arrow_forward_ios, color: isDark ? Colors.white54 : Colors.grey),
+      trailing:
+      Icon(Icons.arrow_forward_ios, color: isDark ? Colors.white54 : Colors.grey),
       onTap: onTap,
     );
   }
